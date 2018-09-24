55acfaa92bd91ab22732c399201a7ea4132da3a1
Teerachai Laothong <tlaothong@gmail.com>
2018-09-24 10:43:56 +0700

    Demo github integration

---
+++ src/githubdeploy/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace githubdeploy.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
